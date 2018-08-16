34f8ff1b478a9c78886c70a8ddd53eacf4070fa0
Ian Hutcheson <penelope15@gmail.com>
2018-08-15 21:40:46 -0700

    Add source code file Pointers.c

---
+++ Pointers.c/Pointers.c
@@ -0,0 +1,20 @@
+// Filename: Pointers.c
+//
+// These are notes and sandbox code for the CS50 2018 Week 3 short video
+// entitled "Pointers".
+//
+// Date: 08/15/2018
+//
+// Name: Ian Hutcheson
+//
+// IDE: Microsoft Visual Studio 2017
+
+
+#include <stdio.h>
+
+void main() {
+
+	printf("Hello, world.");
+
+	return;
+}
